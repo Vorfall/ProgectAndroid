bddf20cefdb75ce9a1d399acae317c9b745531d9
Vorfall <alekseevegorc@gmail.com>
2021-03-11 20:38:46 +0300

    init:Start solution

--- Vorfall/app/src/main/java/com/example/progectandroid/MainActivity.kt
+++ Vorfall/app/src/main/java/com/example/progectandroid/MainActivity.kt
@@ -1,23 +1,11 @@
-package com.example.testfragment
+package com.example.progectandroid
 
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-
-
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-
-    fun Change(View: View){
-        Fragment fragment
-
-    }
-
 }
