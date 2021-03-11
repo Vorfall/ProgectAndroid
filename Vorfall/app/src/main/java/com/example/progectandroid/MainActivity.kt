d8285e941d2d6081f2d3adfe41b74e05a88998c7
Vorfall <alekseevegorc@gmail.com>
2021-03-11 23:34:05 +0300

    feat: creat bar, program crash

--- Vorfall/app/src/main/java/com/example/progectandroid/MainActivity.kt
+++ Vorfall/app/src/main/java/com/example/progectandroid/MainActivity.kt
@@ -3,14 +3,74 @@ package com.example.progectandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup
+import android.view.LayoutInflater
+import androidx.appcompat.widget.Toolbar
 import com.example.progectandroid.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import java.sql.Driver
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+
+
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        creatHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+       mDrawer = DrawerBuilder()
+           .withActivity(this)
+           .withToolbar(mToolbar)
+           .withActionBarDrawerToggle(true)
+           .withSelectedItem(-1)
+           .withAccountHeader(mHeader)
+           .addDrawerItems(
+               PrimaryDrawerItem().withIdentifier(100)
+                   .withIconTintingEnabled(true)
+                   .withName("Create group")
+                   .withSelectable(false)
+           ).build()
+    }
+
+    private fun creatHeader() {
+        mHeader=AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Egor")
+                    .withEmail("egor@gmail.com")
+            ).build()
+
+
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolBar
+
     }
 }
