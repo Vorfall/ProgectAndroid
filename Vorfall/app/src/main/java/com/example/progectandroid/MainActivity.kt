a1f9a1072aa98d0f671f8b74128dfe7a356897ec
Vorfall <alekseevegorc@gmail.com>
2021-03-11 21:08:05 +0300

    fix: version gradle. add test

--- Vorfall/app/src/main/java/com/example/progectandroid/MainActivity.kt
+++ Vorfall/app/src/main/java/com/example/progectandroid/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.progectandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import com.example.progectandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
